f266ebdfa55da09490bc4f7e94cab5b92fc7889c
cicero.isaac <ciceroisaacsc@gmail.com>
2023-12-12 15:25:40 -0300

    Corrigindo bugs do React-Router 2/2

---
+++ movies-lib/src/components/navbar.jsx
@@ -0,0 +1,13 @@
+export const Navbar = () => {
+
+    return(
+
+    <nav id='navbar'>
+        <h2>
+            <Link to="/">Movies Lib</Link>
+        </h2>
+    </nav>
+
+    )
+
+}
---
+++ movies-lib/src/pages/Movie.jsx
@@ -0,0 +1,11 @@
+export const Movie = () => {
+
+    return(
+
+        <div>
+            Movie
+        </div>
+
+    )
+
+}
