740f6bc6bed2bf12b3a44a35d5ffe7da2bef7836
cicero.isaac <ciceroisaacsc@gmail.com>
2023-12-12 16:24:17 -0300

    Organizando hook de Links e useState

--- movies-lib/src/pages/Home.jsx
+++ movies-lib/src/pages/Home.jsx
@@ -1,9 +1,32 @@
+import { useState, useEffect } from "react";
+
+const moviesUrl = import.meta.env.VITE_API
+const apIKey = import.meta.env.VITE_API_KEY
+
 export const Home = () => {
+
+    const [topMovies, setTopMovies] = useState([])
+
+    const getTopRatedMovies = async (url) => {
+
+        const res = await fetch(url)
+        const data = await res.json()
+
+        console.log(data.results);
+    };
+
+    useEffect(() => {
+
+        const topRatedUrl = `${moviesUrl}top_rated${apiKey}`
+
+        console.log(topRatedUrl)
+
+    }, [])
 
     return(
 
         <div>
-            Home
+            {topMovies && topMovies.map((movie) => <p>{movie.title}</p>)}
         </div>
 
     )
